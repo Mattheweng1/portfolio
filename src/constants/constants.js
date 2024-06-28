e7a92b8f653dbb65bbeabef90ad11c06f1283290
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-06-28 18:14:58 -0500

    Add some constant variables

---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const ABOUT_TEXT = `From the beginning, programming has been a fullfilling experience, from the first "Hello World" and through all the websites that came after. Bugs and issues are bound to come up, and they can be stressful, but I like to believe they're also what can make a project's completion so satisfying. Web development isn't always sunshine and butterflies, but when you have to make it through a dragon's den, that's how you know it's an adventure.
+
+Most of my programming education was guided by The Odin Project, an incredible open source coding curriculum that emphasizes hands-on, project-based learning (https://www.theodinproject.com/about). They would give lessons on important topics through videos and articles, but they would always give projects that required more than they taught. It was like teaching me how to build a campfire and then throwing me out in the wild. I guess that's why I think of programming as an adventure. Everytime I survive and escape the wilds, it builds my confidence because I know I can make it out again.
+
+I'm comfortable with my Front-End Development skills now, but the adventure never ends. Let's go on an adventure together!`;
+
+export const EXPERIENCES = [
+  {
+    year: "2021 - Present",
+    role: "Assistant Real Estate Manager",
+    company: "1635 Executive Office Rentals LLC",
+    description: `Staged and marketed rental properties to minimize tenant vacancies. Also handled general tenant-related matters.`,
+  },
+  {
+    year: "2020 - 2021",
+    role: "Youth Leader",
+    company: "LEARN",
+    description: `Worked on a team to supervise and assist students through school days with online class.`,
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Battleship",
+    image: "URL",
+    description:
+      "This project is a web-based implementation of the classic Battleship game where you play against an intelligent AI that attacks using probability maps. Care to challenge the computer?",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Weather App",
+    image: "URL",
+    description:
+      "This is a simple weather app that can show you the forecast for locations all over the world. Wondering what the forecast is?",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: "URL",
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information. It's the website you're on right now!",
+    technologies: ["HTML", "React", "Tailwind", "Framer Motion"],
+  },
+];
+
+export const CONTACT = {
+  address: "2549 Anne Lane, Northbrook, IL 60062",
+  phoneNo: "(224) 639-5166",
+  email: "mattheweng1@yahoo.com",
+};

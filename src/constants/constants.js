0900c3ac4912f9c61380557191a8dec4b1bd53ee
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-06-29 01:20:21 -0500

    Setup 'About' layout

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,9 @@
-export const ABOUT_TEXT = `From the beginning, programming has been a fullfilling experience, from the first "Hello World" and through all the websites that came after. Bugs and issues are bound to come up, and they can be stressful, but I like to believe they're also what can make a project's completion so satisfying. Web development isn't always sunshine and butterflies, but when you have to make it through a dragon's den, that's how you know it's an adventure.
-
-Most of my programming education was guided by The Odin Project, an incredible open source coding curriculum that emphasizes hands-on, project-based learning (https://www.theodinproject.com/about). They would give lessons on important topics through videos and articles, but they would always give projects that required more than they taught. It was like teaching me how to build a campfire and then throwing me out in the wild. I guess that's why I think of programming as an adventure. Everytime I survive and escape the wilds, it builds my confidence because I know I can make it out again.
-
-I'm comfortable with my Front-End Development skills now, but the adventure never ends. Let's go on an adventure together!`;
+export const ABOUT_TEXT = {
+  part1: `From the beginning, programming has been a fullfilling experience, from the first "Hello World" and through all the websites that came after. Bugs and issues are bound to come up, and they can be stressful, but I like to believe they're also what can make a project's completion so satisfying. Web development isn't always sunshine and butterflies, but when you have to make it through a dragon's den, that's how you know it's an adventure.`,
+  part2: `Most of my programming education was guided by The Odin Project, an incredible open source coding curriculum that emphasizes hands-on, project-based learning. They provide lessons on important topics through videos and articles, but they always give projects that require more than they teach.`,
+  part3: `It was like teaching me how to build a campfire and then throwing me out in the wild. That's probably why I think of programming as an adventure. Everytime I survive and escape the wilds, it builds my confidence because I know I can make it out again.`,
+  part4: `I'm comfortable with my Front-End Development skills now, but the adventure never ends. Let's go on an adventure together!`,
+};
 
 export const EXPERIENCES = [
   {

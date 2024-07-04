5c9e53d71ad6dd83f4614ca8fe9342ee3db1cb3c
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-07-04 00:23:21 -0500

    Change About content & layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,4 @@
-import { ABOUT_TEXT } from "../constants/constants";
-import Button from "./Button";
 import FlexWrapper from "./FlexWrapper";
-import FlexWrapperItem from "./FlexWrapperItem";
 import Navbar from "./Navbar";
 import PageContainer from "./PageContainer";
 import SectionTitle from "./SectionTitle";
@@ -13,57 +10,49 @@ const About = () => {
       <PageContainer>
         <SectionTitle title="About My"></SectionTitle>
         <FlexWrapper>
-          <FlexWrapperItem className="p-8">
-            <img
-              className="rounded-2xl"
-              src="src/assets/about.jpg"
-              alt="Portrait Photo of Matthew"
-            />
-          </FlexWrapperItem>
-          <FlexWrapperItem>
-            <p className="text-center lg:text-left">{ABOUT_TEXT.part1}</p>
-          </FlexWrapperItem>
+          <div className="max-w-xs text-center m-6">
+            <h1 className="text-red-500 my-6 text-3xl font-light">Education</h1>
+            <p className="text-xl">
+              I learned most of my front-end development skills through
+              hands-on, project-based learning with{" "}
+              <a
+                href="http://theodinproject.com/about"
+                className="text-red-500"
+              >
+                The Odin Project
+              </a>
+              . It taught me many concepts, but most importantly, it taught me
+              how to problem solve.
+            </p>
+          </div>
+          <div className="max-w-xs text-center m-6">
+            <h1 className="text-red-500 my-6 text-3xl font-light">Interests</h1>
+            <p className="text-xl">
+              Within programming, I&apos;m particularly interested in techniques
+              to make aesthetic and user-friendly experiences. I also find
+              algorithms to be fascinating. Aside from programming, I love
+              volleyball, hiking, and gaming.
+            </p>
+          </div>
+          <div className="max-w-xs text-center m-6">
+            <h1 className="text-red-500 my-6 text-3xl font-light">
+              Aspirations
+            </h1>
+            <p className="text-xl">
+              Looking ahead, I&apos;m excited to continue refining my front-end
+              development skills, but I also plan to learn back-end. For life
+              goals, I hope to fulfill my dream of living in different places
+              across the world while working remotely.
+            </p>
+          </div>
         </FlexWrapper>
-        <FlexWrapper className="flex-row-reverse">
-          <FlexWrapperItem className="p-8">
-            <img
-              className="rounded-2xl"
-              src="src/assets/about.jpg"
-              alt="Portrait Photo of Matthew"
-            />
-          </FlexWrapperItem>
-          <FlexWrapperItem>
-            <p className="text-center lg:text-left">{ABOUT_TEXT.part2}</p>
-          </FlexWrapperItem>
-        </FlexWrapper>
+        <SectionTitle title="Skills" />
+        <div>Skill Icons</div>
         <FlexWrapper>
-          <FlexWrapperItem className="p-8">
-            <img
-              className="rounded-2xl"
-              src="src/assets/about.jpg"
-              alt="Portrait Photo of Matthew"
-            />
-          </FlexWrapperItem>
-          <FlexWrapperItem>
-            <p className="text-center lg:text-left">{ABOUT_TEXT.part3}</p>
-          </FlexWrapperItem>
-        </FlexWrapper>
-        <FlexWrapper className="flex-row-reverse">
-          <FlexWrapperItem className="p-8">
-            <img
-              className="rounded-2xl"
-              src="src/assets/about.jpg"
-              alt="Portrait Photo of Matthew"
-            />
-          </FlexWrapperItem>
-          <FlexWrapperItem>
-            <p className="text-center lg:text-left">{ABOUT_TEXT.part4}</p>
-            <Button
-              route="/contact"
-              text="Hire me!"
-              className="lg:self-start"
-            />
-          </FlexWrapperItem>
+          <div>HTML</div>
+          <div>CSS</div>
+          <div>JavaScript</div>
+          <div>React</div>
         </FlexWrapper>
       </PageContainer>
     </>

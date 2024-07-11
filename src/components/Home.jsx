3dc3e6ebfae5d48f3b54615277f4a6d5737b49e8
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-07-10 22:48:21 -0500

    Add profile photo to Home & tweak appearance

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
+import FlexWrapper from "./FlexWrapper";
 
 const Home = () => {
   return (
-    <div className="flex flex-wrap justify-between items-center gap-x-24 h-screen w-full">
-      <div className="flex-auto m-6 flex justify-center">
-        <h1 className="text-5xl leading-normal ">
+    <FlexWrapper className="items-center gap-x-24 h-screen w-full !m-0 [text-shadow:_1px_2px_5px_var(--tw-shadow-color)] shadow-black">
+      <div className="flex-auto m-6 flex justify-start w-full lg:w-auto lg:relative">
+        <h1 className="text-5xl leading-normal">
           Hi,
           <br />
           I&apos;m{" "}
@@ -16,15 +17,21 @@ const Home = () => {
             E
           </span>
           ng
-          <br />a web developer
+          <br />
+          <span className="text-red-700">a web developer</span>
         </h1>
+        <img
+          src="src/assets/Profile Photo 2023.JPEG"
+          alt="Profile Photo"
+          className="absolute top-5 right-10 md:right-[20%] lg:top-auto lg:-bottom-[10%] lg:right-0 -z-10 h-[40vh] opacity-50 border-2"
+        />
       </div>
       <div className="flex-auto m-6 flex flex-col gap-20 items-center text-5xl">
         <NavLink to="/about">About</NavLink>
         <NavLink to="/projects">Projects</NavLink>
         <NavLink to="/contact">Contact</NavLink>
       </div>
-    </div>
+    </FlexWrapper>
   );
 };
 export default Home;

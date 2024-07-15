4149c215a0b54b94448ca624a26b8949665bcede
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-07-15 18:12:43 -0500

    Add exit animations to SlideIn

--- src/components/SlideIn.jsx
+++ src/components/SlideIn.jsx
@@ -8,6 +8,8 @@ const SlideIn = ({
   initialY = 0,
   finalOpacity = 1,
   scaleX = false,
+  exitX = 0,
+  exitY = 0,
   duration = 1,
   delay = 0,
   spring = false,
@@ -15,8 +17,14 @@ const SlideIn = ({
   return (
     <motion.div
       className={className}
-      initial={{ x: initialX, y: initialY, opacity: 0, scaleX: scaleX ? 0 : 1 }}
+      initial={{
+        x: initialX,
+        y: initialY,
+        opacity: 0,
+        scaleX: scaleX ? 0 : 1,
+      }}
       animate={{ x: 0, y: 0, opacity: finalOpacity, scaleX: 1 }}
+      exit={{ x: exitX, y: exitY, opacity: 0, scaleX: scaleX ? 0 : 1 }}
       transition={{
         duration: duration,
         ease: "easeOut",
@@ -36,6 +44,8 @@ SlideIn.propTypes = {
   initialY: PropTypes.number,
   finalOpacity: PropTypes.number,
   scaleX: PropTypes.bool,
+  exitX: PropTypes.number,
+  exitY: PropTypes.number,
   duration: PropTypes.number,
   delay: PropTypes.number,
   spring: PropTypes.bool,

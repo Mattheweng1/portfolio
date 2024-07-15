44dc42b5636cb16dbeea6860307874f3ac9fb9c9
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-07-15 17:07:49 -0500

    Stagger TitledCards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,34 +12,36 @@ const About = () => {
   return (
     <>
       <TitledSection title="About My" id="about">
-        <FlexWrapper className="gap-12">
-          <TitledCard title="Education">
-            I learned most of my front-end development skills through hands-on,
-            project-based learning with{" "}
-            <a
-              href={LINKS.theOdinProject}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-red-500"
-            >
-              The Odin Project
-            </a>
-            . It taught me many concepts, but most importantly, it taught me how
-            to problem solve.
-          </TitledCard>
-          <TitledCard title="Interests">
-            Within programming, I&apos;m particularly interested in techniques
-            to make aesthetic and user-friendly experiences. I also find
-            algorithms to be fascinating. Aside from programming, I love
-            volleyball, hiking, and gaming.
-          </TitledCard>
-          <TitledCard title="Aspirations">
-            Looking ahead, I&apos;m excited to continue refining my front-end
-            development skills, but I also plan to learn back-end. For life
-            goals, I hope to fulfill my dream of living in different places
-            across the world while working remotely.
-          </TitledCard>
-        </FlexWrapper>
+        <StaggerBox staggerChildren={0.2}>
+          <FlexWrapper className="gap-12">
+            <TitledCard title="Education">
+              I learned most of my front-end development skills through
+              hands-on, project-based learning with{" "}
+              <a
+                href={LINKS.theOdinProject}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-red-500"
+              >
+                The Odin Project
+              </a>
+              . It taught me many concepts, but most importantly, it taught me
+              how to problem solve.
+            </TitledCard>
+            <TitledCard title="Interests">
+              Within programming, I&apos;m particularly interested in techniques
+              to make aesthetic and user-friendly experiences. I also find
+              algorithms to be fascinating. Aside from programming, I love
+              volleyball, hiking, and gaming.
+            </TitledCard>
+            <TitledCard title="Aspirations">
+              Looking ahead, I&apos;m excited to continue refining my front-end
+              development skills, but I also plan to learn back-end. For life
+              goals, I hope to fulfill my dream of living in different places
+              across the world while working remotely.
+            </TitledCard>
+          </FlexWrapper>
+        </StaggerBox>
       </TitledSection>
       <TitledSection title="Skills" id="skills">
         <SlideInView initialX={-50}>
--- src/components/TitledCard.jsx
+++ src/components/TitledCard.jsx
@@ -1,18 +1,23 @@
 import PropTypes from "prop-types";
-import SlideInView from "./SlideInView";
+import StaggerItem from "./StaggerItem";
 
 const TitledCard = ({ title, children }) => {
   return (
-    <SlideInView className="max-w-xs text-center" initialX={-50}>
+    <StaggerItem
+      className="max-w-xs text-center"
+      initialX={-50}
+      duration={0.75}
+    >
       <h1 className="text-red-500 mb-6 text-3xl font-light">{title}</h1>
       <p className="text-xl">{children}</p>
-    </SlideInView>
+    </StaggerItem>
   );
 };
 
 TitledCard.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  delay: PropTypes.number,
 };
 
 export default TitledCard;

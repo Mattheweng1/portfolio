b9e92a43769815b1490fd152e1a37b775b32fd27
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-07-14 23:39:34 -0500

    Add SlideIn to NavIcons

--- src/components/NavIcons.jsx
+++ src/components/NavIcons.jsx
@@ -1,17 +1,28 @@
 import PropTypes from "prop-types";
 import { FaGithub, FaLinkedin } from "react-icons/fa6";
 import { LINKS } from "../constants/constants";
+import SlideIn from "./SlideIn";
 
 const NavIcons = ({ className }) => {
   return (
     <div className={"flex items-center gap-10 text-4xl " + className}>
-      <a href={LINKS.linkedIn} target="_blank" rel="noopener noreferrer">
-        <FaLinkedin />
-      </a>
-      <a href={LINKS.gitHub} target="_blank" rel="noopener noreferrer">
-        <FaGithub />
-      </a>
-      <div className="h-1 rounded-sm flex-auto bg bg-neutral-300"></div>
+      <SlideIn initialY={-100} duration={0.4} delay={1} spring>
+        <a href={LINKS.linkedIn} target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+      </SlideIn>
+      <SlideIn initialY={-100} duration={0.4} delay={1.3} spring>
+        <a href={LINKS.gitHub} target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+      </SlideIn>
+      <SlideIn
+        className="h-1 rounded-sm flex-auto bg bg-neutral-300"
+        initialX={"50%"}
+        scaleX
+        duration={1}
+        spring
+      ></SlideIn>
     </div>
   );
 };

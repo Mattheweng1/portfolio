755869307179e9714fd91b63ce4d84c17ab32587
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-07-16 04:13:10 -0500

    Adjust text responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,9 +12,9 @@ const Home = () => {
   return (
     <div id="home" className="flex flex-col h-screen w-full">
       <div className="flex flex-auto flex-col lg:flex-row gap-x-24 [text-shadow:_1px_2px_3px_var(--tw-shadow-color)] shadow-black">
-        <div className="flex-auto m-6 flex items-end min-h-[35vh] lg:self-center lg:relative">
+        <div className="flex-auto sm:m-6 flex items-end min-h-[35vh] lg:self-center lg:relative">
           <SlideIn
-            className="text-4xl sm:text-5xl leading-normal"
+            className="text-3xl sm:text-5xl leading-normal"
             initialX={-100}
             duration={1}
             spring
@@ -24,14 +24,14 @@ const Home = () => {
             <span className="block sm:inline">I am </span>
             <span className="inline-block">
               <DropIn
-                className="text-red-500 text-7xl leading-[0.74] font-bold"
+                className="text-red-500 text-5xl sm:text-7xl leading-[0.74] font-bold"
                 delay={1}
               >
                 M
               </DropIn>
               atthew{" "}
               <DropIn
-                className="text-red-500 text-7xl leading-[0.74] font-bold"
+                className="text-red-500 text-5xl sm:text-7xl leading-[0.74] font-bold"
                 delay={1.3}
               >
                 E
@@ -53,7 +53,7 @@ const Home = () => {
             <img src={profilePhoto} alt="Profile Photo" className="h-full" />
           </SlideIn>
         </div>
-        <div className="flex-auto m-6 flex justify-end text-5xl font-light lg:h-[35vh] lg:self-center">
+        <div className="flex-auto sm:m-6 flex justify-end text-4xl sm:text-5xl font-light lg:h-[35vh] lg:self-center">
           <StaggerBox
             className="flex flex-col justify-evenly lg:justify-between text-right"
             onView
